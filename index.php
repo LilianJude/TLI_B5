499c6fec102cfa910a4ff4e27ec8b8fd45b00bb3
Jude Lilian <lilian.jude2@gmail.com>
2019-09-09 13:49:55 +0200

    renommage de fichiers

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   if (isset($_GET['page']) && file_exists(_CTRL_.str_replace('.', '', $_GET['page']).'.php'))
       include(_CTRL_.$_GET['page'].'.php');
   else
-      include(_CTRL_.'index.php');
+      include(_CTRL_.'home.php');
 
   // Affichage des templates
   $smarty->display(_TPL_ . 'header.tpl');
@@ -18,7 +18,7 @@
   if (isset($_GET['page']) && file_exists(_TPL_.'pages/'.str_replace('.', '', $_GET['page']).'.tpl'))
       $smarty->display(_TPL_.'pages/'.$_GET['page'].'.tpl');
   else
-      $smarty->display(_TPL_ . 'pages/index.tpl');
+      $smarty->display(_TPL_ . 'pages/home.tpl');
 
   $smarty->display(_TPL_ . 'footer.tpl');
 ?>
---
+++ templates_c/8da691eab7b688f4305f79480e69ae21605b6f4b_0.file.home.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-09-09 11:47:19
+  from 'C:\wamp64\www\TLI_B5\tpl\pages\home.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5d763bc7e77e94_14709645',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8da691eab7b688f4305f79480e69ae21605b6f4b' => 
+    array (
+      0 => 'C:\\wamp64\\www\\TLI_B5\\tpl\\pages\\home.tpl',
+      1 => 1567674601,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5d763bc7e77e94_14709645 (Smarty_Internal_Template $_smarty_tpl) {
+?><div>
+      Le contenu de notre page index.tpl
+      <?php echo $_smarty_tpl->tpl_vars['nomDeMaVariableSousSmarty']->value;?>
+
+</div>
+<?php }
+}
