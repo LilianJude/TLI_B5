922277fe88a68bce749529012094f33a1583fc0f
Jeremy Mougey <35655138+empyrz@users.noreply.github.com>
2019-10-03 15:31:21 +0200

    Some updates

--- connect.php
+++ connect.php
@@ -28,6 +28,7 @@ define('MYSQL_DATABASE', 'acu');
  */
 $pdoOptions = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
     PDO::ATTR_EMULATE_PREPARES => false
 );
 
