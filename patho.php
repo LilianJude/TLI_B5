b71d03984ea87da16a9f2bdcab7488e02d9b0bfd
Jeremy Mougey <35655138+empyrz@users.noreply.github.com>
2019-10-23 11:23:54 +0200

    Pathologies filters

--- patho.php
+++ patho.php
@@ -20,7 +20,6 @@ include 'connect.php';
 <head>
 	<meta charset="utf-8">
 	<title>Pathologies</title>
-	<link rel="stylesheet" href="style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script src="js/script.js"></script>
 	<script src="js/kickstart.js"></script>
@@ -64,19 +63,102 @@ include 'connect.php';
 			}
 		?>
 		</select><br/><br/>
-		<label for="select1">Méridiens :</label>
-		<p>
-		<select>
-		<?php
-			$reponse = $pdo->prepare('SELECT * FROM `meridien`');
-			$reponse->execute();
-			#$reponse = $pdo->query('select S.descr from symptome S join symptpatho SP on SP.idS = S.idS join patho P on P.idP = SP.idP where p.idp = $value');
-			// On affiche chaque entrée une à une
-			while ($donnees = $reponse->fetch()){
-				echo '<option value="'.$donnees['code'].'">'.$donnees['nom'].'</option>';
-			}
-		?>
-		</select>
+		<div id="filters">
+			<fieldset id="fs_mer">
+				<legend>Méridiens et Merveilleux Vaisseaux</legend>
+				
+				<div class="inputMer">
+					<ul>
+						
+						<li><input type="checkbox" name="f_P" id="f_P"/><label for="f_P">Poumon</label></li>
+						
+						<li><input type="checkbox" name="f_GI" id="f_GI"/><label for="f_GI">Gros Intestin</label></li>
+						
+						<li><input type="checkbox" name="f_E" id="f_E"/><label for="f_E">Estomac</label></li>
+						
+						<li><input type="checkbox" name="f_Rte" id="f_Rte"/><label for="f_Rte">Rate/Pancréas</label></li>
+						
+					</ul>
+				</div>
+				
+				<div class="inputMer">
+					<ul>
+						
+						<li><input type="checkbox" name="f_C" id="f_C"/><label for="f_C">Cœur</label></li>
+						
+						<li><input type="checkbox" name="f_IG" id="f_IG"/><label for="f_IG">Intestin grêle</label></li>
+						
+						<li><input type="checkbox" name="f_V" id="f_V"/><label for="f_V">Vessie</label></li>
+						
+						<li><input type="checkbox" name="f_R" id="f_R"/><label for="f_R">Rein</label></li>
+						
+					</ul>
+				</div>
+				
+				<div class="inputMer">
+					<ul>
+						
+						<li><input type="checkbox" name="f_MC" id="f_MC"/><label for="f_MC">Maître du cœur</label></li>
+						
+						<li><input type="checkbox" name="f_TR" id="f_TR"/><label for="f_TR">Triple réchauffeur</label></li>
+						
+						<li><input type="checkbox" name="f_VB" id="f_VB"/><label for="f_VB">Vésicule Biliaire</label></li>
+						
+						<li><input type="checkbox" name="f_F" id="f_F"/><label for="f_F">Foie</label></li>
+						
+					</ul>
+				</div>
+				
+				<div class="inputMer">
+					<ul>
+						
+						<li><input type="checkbox" name="f_DM" id="f_DM"/><label for="f_DM">Du Mai</label></li>
+						
+						<li><input type="checkbox" name="f_RM" id="f_RM"/><label for="f_RM">Ren Mai</label></li>
+						
+						<li><input type="checkbox" name="f_ChM" id="f_ChM"/><label for="f_ChM">Chong Mai</label></li>
+						
+						<li><input type="checkbox" name="f_DaiM" id="f_DaiM"/><label for="f_DaiM">Dai Mai</label></li>
+						
+					</ul>
+				</div>
+				
+				<div class="inputMer">
+					<ul>
+						
+						<li><input type="checkbox" name="f_+QM" id="f_+QM"/><label for="f_+QM">Yang Qiao Mai</label></li>
+						
+						<li><input type="checkbox" name="f_-QM" id="f_-QM"/><label for="f_-QM">Yin Qiao Mai</label></li>
+						
+						<li><input type="checkbox" name="f_+WM" id="f_+WM"/><label for="f_+WM">Yang Wei Mai</label></li>
+						
+						<li><input type="checkbox" name="f_-WM" id="f_-WM"/><label for="f_-WM">Yin Wei Mai</label></li>
+						
+					</ul>
+				</div>
+				<br/>
+				<input type="button" value="Tout cocher" onclick="this.value=check('fs_mer')">
+				</fieldset>
+				
+				<fieldset id="fs_patho" class="inputPatho">
+					<legend>Pathologies</legend>
+					<div class="inputPatho">
+						
+						<input type="checkbox" name="fp_me" id="fp_me"/><label for="fp_me">Méridien externe</label>
+						
+						<input type="checkbox" name="fp_mi" id="fp_mi"/><label for="fp_mi">Méridien interne</label>
+						
+						<input type="checkbox" name="fp_tf" id="fp_tf"/><label for="fp_tf">Zang/Fu</label>
+						
+						<input type="checkbox" name="fp_l" id="fp_l"/><label for="fp_l">Voie Luo</label>
+						
+						<input type="checkbox" name="fp_j" id="fp_j"/><label for="fp_j">Jin Jing</label>
+						
+						<input type="checkbox" name="fp_mv" id="fp_mv"/><label for="fp_mv">Merveilleux Vaisseaux</label>
+						
+					</div>
+				</fieldset>
+		</div>
 		<?php
 		if(!(isset($_SESSION['user_id']) || isset($_SESSION['logged_in']))) { ?>
 			<p>Veuillez vous connecter pour accéder à la recherche de pathologie par mot-clé</p>
@@ -102,14 +184,11 @@ include 'connect.php';
 					?>
 					<label for="select2">Pathologie : </label>
 					<p>
-					<select>
 					<?php
 					while ($donnees = $reponse->fetch()){
-						echo '<option value="'.$donnees['idP'].'">'.$donnees['description'].'</option>';
+						echo '<li>'.$donnees['description'].'</li>';
 						}
-
 					?>
-					</select>
 					</p>
 					<br/>
 				<?php 
